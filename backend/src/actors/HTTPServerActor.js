e85b84f94d6c79deb9b1196992f8aa973d5beb76
Ivan Malone <ivanincanada@gmail.com>
2019-02-05 09:20:03 -0800

    Initial commit

---
+++ backend/src/actors/HTTPServerActor.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+class HTTPServerActor
+  {
+    // our constuctor is called each time a new instance of our class is created
+  	  initialize(selfActor)
+  			{
+  			this.selfActor = selfActor;
+
+  			selfActor.getLog().info("Actor initialize [" + selfActor + "]");
+  			return 	this.selfActor;
+  			}
+
+        setup(activePort)
+    			{
+    				this.selfActor.getLog().info("Actor setup for [" + this.selfActor.name + "] on port " + activePort);
+
+
+    				// lets change the application's title, so it is reflected in terminal
+    				process.title = applicationTitle;
+
+      		}
+
+          displayStartupInfo()
+        		{
+        		// here we will use 'process' to retrieve some useful information about our environment
+        		// See https://nodejs.org/api/process.html - to see what else you can do with nodejs's 'process'
+
+        		// this indicates which 'nodejs' installation is running our application
+        		this.selfActor.getLog().info("Node Path: " + process.argv[0]);
+          }
+  }
+
+  module.exports = HTTPServerActor
---
+++ xxx.sh
@@ -0,0 +1,5 @@
+git init
+git remote add origin git@873a8522-c495-433a-9f18-34de946733f6.pub.cloud.scaleway.com:cornerstone/ActorProject.git
+git add .
+git commit -m "Initial commit"
+git push -u origin master
